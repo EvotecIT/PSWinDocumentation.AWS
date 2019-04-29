27a77e0013146ad68e34695d3371c669d125609b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-29 13:25:39 +0200

    Update

--- Private/Get-AWSSubnetDetails.ps1
+++ Private/Get-AWSSubnetDetails.ps1
@@ -12,13 +12,20 @@ function Get-AWSSubnetDetails {
         Write-Warning "Get-AWSSubnetDetails - Error: $ErrorMessage"
         return
     }
+    try {
+        $VPCID = (Get-EC2Vpc -AccessKey $AWSAccessKey -SecretKey $AWSSecretKey -Region $AWSRegion)
+    } catch {
+        $ErrorMessage = $_.Exception.Message
+        Write-Warning "Get-AWSSubnetDetails - Error: $ErrorMessage"
+        return
+    }
     $NetworkDetailsList = foreach ($subnet in $Subnets) {
         $SN = [pscustomobject] @{
             "Subnet ID"    = $subnet.SubnetId
             "Subnet Name"  = $subnet.Tags | Where-Object {$_.key -eq "Name"} | Select-Object -Expand Value
             "CIDR"         = $subnet.CidrBlock
             "Available IP" = $subnet.AvailableIpAddressCount
-            "VPC"          = (Get-EC2Vpc -VpcId $subnet.VpcId -AccessKey $AWSAccessKey -SecretKey $AWSSecretKey -Region $AWSRegion).Tags | Where-Object {$_.key -eq "Name"} | Select-Object -Expand Value
+            "VPC"          = ($VPCID | Where-Object { $_.VpcId -eq $Subnet.VpcId }).Tags | Where-Object {$_.key -eq "Name"} | Select-Object -Expand Value
         }
         $SN
     }
