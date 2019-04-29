9f13fcacc55d9a3f69632f5102d93738bed37d23
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-29 13:26:42 +0200

    Update

---
+++ Examples/RunMe.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module AWSPowerShell
+Import-Module .\PSWinDocumentation.AWS.psd1 -Force
+
+$Configuration = [ordered] @{
+    AWSAccessKey = ''
+    AWSSecretKey = ''
+    AWSRegion    = 'eu-west-1'
+}
+
+$AWS = Get-WinAWSInformation -AWSAccessKey $Configuration.AWSAccessKey -AWSSecretKey $Configuration.AWSSecretKey -AWSRegion $Configuration.AWSRegion
+$AWS
